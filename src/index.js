fa36e4e2e0b862f3e67ec8300da834a37b9bec27
Iwuchkwu O <kizob123@gmail.com>
2022-09-02 05:51:22 +0100

    third commit of malon app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,118 @@
 let album = []
-let fetch_album = async function () {
- 
-    let fs =await fetch("http://jsonplaceholder.typicode.com/users/1/albums")
+$(function () {
+     $(".cmm").on('click', function (e) {
+         //$('.modal').modal('show')
+         $('.log-modal').modal('show')
+        
+     })
+    $(".login").on('click',function(e){
+    //$('.modal').modal('show')
+    $('.log-modal').modal('show')
+    })
+    $(".register").on('click', function (e) {
+        //$('.modal').modal('show')
+        $('.reg-modal').modal('show')
+    })
+   
+    $('.modal-close').on('click', function (e) {
+        $('.modal').modal('hide')
+    })
+    // $('.modal').on('shown.bs.modal', function () {
+    //     $('#myInput').trigger('focus')
+    // })
+    $('.album-n').on('mousedown', function (e) {
+        let n= $(this).attr('value')
+        document.querySelector('.folder-photo-album').innerHTML = ""
+        fetch_album(n)
+    })
+     $('#login-form').on('submit', async function (e) {
+         e.preventDefault()
+         console.log(e.currentTarget)
+         const formData = new FormData(e.currentTarget)
+         const objFormData = Object.fromEntries(formData.entries())
+         
+         const jsonFormData = JSON.stringify(objFormData)
+         const res = await fetch(`https://jsonplaceholder.typicode.com/users?email=${objFormData['email ']}&username=${objFormData['username ']}`, {
+             method: 'GET',
+             headers: {
+                 'Content-type': 'application/json; charset=UTF-8',
+             },
+         })
+
+         if (!res.ok) {
+             const err = await res.text()
+             throw new Error(err)
+         }
+        res.json().then(
+            data => {
+                console.log(data);
+                let n = Math.ceil(Math.random() * 14)
+                $('.pr_p').attr('src', `./img/Imgs/img${n}.png`)
+                $('.login,.register').css('display','none')
+                $('.sign-o').css('display', 'block')
+                $('.pr_name').css('display', 'block').text(data[0].name)
+                $('.log-modal').modal('hide')
+            }
+        )
+     })
+    $('#user-form').on('submit', async function (e) {
+        e.preventDefault()
+        console.log(e.currentTarget)
+        const formData = new FormData(e.currentTarget)
+        const objFormData = Object.fromEntries(formData.entries())
+        const jsonFormData = JSON.stringify(objFormData)
+        const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: jsonFormData,
+            mode:"cors",
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                
+            },
+        })
+
+        if (!res.ok) {
+            const err = await res.text()
+            throw new Error(err)
+        }
+         res.json().then(
+             data=>{
+                 let n=Math.ceil(Math.random()*14)
+                 $('.pr_p').attr('src',`./img/Imgs/img${n}.png`)
+                 $('.login,.register').css('display', 'none')
+                 $('.sign-o').css('display', 'block')
+                 $('.pr_name').css('display', 'block')
+                 $('.reg-modal').modal('hide')
+             }
+         )
+    })
+    
+
+})
+
+
+
+let fetch_album = async function (id) {
+    let fs=null
+    if(id!=='')
+    fs =await fetch(`http://jsonplaceholder.typicode.com/users/1/albums?id=${id}`)
+    else
+    fs = await fetch(`http://jsonplaceholder.typicode.com/users/1/albums`)
     let rs = await fs.json().then(data=>{
         let album_holder = document.querySelector('.album-number-holder')
         data.forEach(element => {
             let single_album = document.createElement('a')
-            single_album.setAttribute('class', 'btn btn-secondary btn-md fw-bold mt-1')
+            console.log(window.innerWidth);
+            single_album.setAttribute('class', 'album-n btn btn-secondary btn-md fw-bold mt-1')
             single_album.setAttribute('role', 'button')
+            single_album.setAttribute('href', '#')
+            single_album.setAttribute('value', element.id)
+            if (window.innerWidth>800)
             single_album.innerText = 'album ' + element.id;
+            else{
+            document.querySelector('.album-top').setAttribute('style','display:block')
+            single_album.innerText = element.id;
+        }
             album_holder.appendChild(single_album)
             fetch_posts(element.id)
         });
@@ -57,7 +161,8 @@ let fetch_posts = async function (id) {
     let rs_img = await fs_img.json().then(data=>{
         let img = document.createElement('img')
         img.setAttribute('class','card-img-top')
-        img.setAttribute('src',''+data.url)
+        let n = Math.ceil(Math.random() * 14)+1
+        img.setAttribute('src', `./img/Imgs/img${n}.png`)
         post_holder.appendChild(img)
     })
     let fs_p = await fetch(`http://jsonplaceholder.typicode.com/posts/${id}`)
@@ -66,16 +171,19 @@ let fetch_posts = async function (id) {
         
         
         
-        let post_ = document.createElement('card-body')
+        let post_ = document.createElement('div')
+        post_.setAttribute('class','card-body')
         
             let single_post_title = document.createElement('div')
             single_post_title.setAttribute('class', 'card-title fw-bold fs-13')
             single_post_title.innerText=data.title
             let single_post = document.createElement('p')
-            single_post.setAttribute('class', 'card-text')
+            single_post.setAttribute('class', 'card-text post-box')
             single_post.innerText=data.body
             let comment_btn = document.createElement('a')
-            comment_btn.setAttribute('class','btn btn-primary fs-9')
+            comment_btn.setAttribute('class','btn btn-primary fs-9 cmm')
+            
+            comment_btn.setAttribute('href', '#')
             comment_btn.innerText = 'Comment'
             post_.appendChild(single_post_title)
             post_.appendChild(document.createElement('hr'))
@@ -91,6 +199,6 @@ let fetch_posts = async function (id) {
 }
 
 window.onload=function(){
-    fetch_album()
+    fetch_album('')
     
 }
